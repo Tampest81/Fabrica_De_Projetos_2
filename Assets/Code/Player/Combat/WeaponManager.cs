92d1dada98406d17d2c529fb62952bb9f002e81a
Pedro Ghiotti Martins <pedro.ghiotti.m@gmail.com>
2020-08-19 21:23:51 -0300

    Mudança nos tiros.
     - Tiros agora acertam inimigos!
     - Tiros passaram de projéteis a hitscan.

---
+++ Assets/Code/Player/Combat/WeaponManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class WeaponManager : MonoBehaviour
+{
+    private Vector2 aimDirection;
+    private float rotZ;
+    private ContactFilter2D enemiesFilter;
+    [SerializeField] private LayerMask enemiesLayer;
+    [SerializeField] private GameObject bulletTrail;
+    private List<RaycastHit2D> enemiesHit = new List<RaycastHit2D>();
+
+    void Start()
+    {
+        enemiesFilter.useLayerMask = true;
+        enemiesFilter.SetLayerMask(enemiesLayer);
+    }
+    void Update()
+    {
+        aimDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position;
+        aimDirection.Normalize();
+        rotZ = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            enemiesHit.Clear();
+            Physics2D.Raycast(this.transform.position, aimDirection, enemiesFilter, enemiesHit, 10);
+            var trailInstance = Instantiate(bulletTrail, this.transform.position, Quaternion.Euler(0, 0, rotZ + -90));
+            trailInstance.transform.localScale = new Vector3(.0625f, 10, 1);
+
+            foreach (var element in enemiesHit)
+            {
+                element.collider.gameObject.GetComponent<TestEnemy>().Damage(1);
+            }
+        }
+    }
+}
---
+++ Assets/TestEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy : MonoBehaviour
+{
+    [SerializeField] private int health = 3;
+    void Update()
+    {
+        switch (health)
+        {
+            case 3:
+                this.GetComponent<SpriteRenderer>().material.color = Color.red;
+                break;
+            case 2:
+                this.GetComponent<SpriteRenderer>().material.color = Color.yellow;
+                break;
+            case 1:
+                this.GetComponent<SpriteRenderer>().material.color = Color.green;
+                break;
+            case 0:
+                health = -1;
+                this.GetComponent<SpriteRenderer>().material.color = Color.clear;
+                Invoke("ResetEnemy", 1);
+                break;
+        }
+    }
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+    }
+    private void ResetEnemy()
+    {
+        health = 3;
+    }
+}
