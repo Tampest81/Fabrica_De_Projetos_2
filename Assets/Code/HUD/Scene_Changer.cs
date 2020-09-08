383b6507abf55cb0aed5d793998c13952a93405c
Alessandro <maquexxx@gmail.com>
2020-09-08 11:23:34 -0300

    yey_HUD
    Adição do script da hud, mas ainda não apliquei na fase. (já está funcionando, imagino q ainda falta colocar mais algumas funcionalidades e arrumar certas coisas, mas a base está feita)

---
+++ Assets/Code/HUD/Scene_Changer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Changer : MonoBehaviour
+{
+    public string Scene1;
+    public string Scene2;
+    public string Scene3;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(Scene1);
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene(Scene2);
+    }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene(Scene3);
+    }
+
+}
