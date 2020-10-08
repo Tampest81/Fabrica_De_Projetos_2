82a2f59ea0beb1c2dba8afc91b0dae025ffc848a
Alessandro <maquexxx@gmail.com>
2020-10-08 12:40:14 -0300

    Novo Menu "Press any button to start"
    adição de um menu que antecede o menu principal.

---
+++ Assets/Code/HUD/Buttton_Scene_Change.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttton_Scene_Change : MonoBehaviour
+{
+    public string scenename;
+
+    Animator animation;
+    public string animationName;
+
+    private void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            animation.Play(animationName);
+        }
+    }
+
+    public void sceneChange()
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
