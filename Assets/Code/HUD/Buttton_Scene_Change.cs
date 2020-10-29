1a1a363a2812e4ed671878603d7fa187b04389b3
Alessandro <maquexxx@gmail.com>
2020-10-29 15:05:38 -0300

    agora tem fade
    agora tem fade in e fade out, só falta as fases para por elas em game, mas já está funcionando nos menus

--- Assets/Code/HUD/Buttton_Scene_Change.cs
+++ Assets/Code/HUD/Buttton_Scene_Change.cs
@@ -8,11 +8,21 @@ public class Buttton_Scene_Change : MonoBehaviour
     public string scenename;
 
     Animator animation;
+
+    public string StartAnim;
     public string animationName;
+    public string DisappearClip;
+    public string Loadinganim;
+
+
+    public string FadeIn;
+    public string FadeOut;
 
     private void Start()
     {
         animation = GetComponent<Animator>();
+        animation.Play(FadeOut);
+        
     }
 
     private void Update()
@@ -26,5 +36,25 @@ public class Buttton_Scene_Change : MonoBehaviour
     public void sceneChange()
     {
         SceneManager.LoadScene(scenename);
+    }
+
+    public void ButtonDisappear()
+    {
+        animation.Play(DisappearClip);
+    }
+
+    public void LoadingAnim()
+    {
+        animation.Play(Loadinganim);
+    }
+
+    public void Fadein()
+    {
+        animation.Play(FadeIn);
+    }
+
+    public void Startanim()
+    {
+        animation.Play(StartAnim);
     }
 }
