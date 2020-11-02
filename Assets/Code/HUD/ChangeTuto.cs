3702e86d0368e219e2e4608b0fb0cc2394e5acc2
Alessandro <maquexxx@gmail.com>
2020-11-02 20:42:54 -0200

    FAADDEEEE
    rgrgrdgd

---
+++ Assets/Code/HUD/ChangeTuto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeTuto : MonoBehaviour
+{
+    public Animator animator;
+    public ChangeSceneMethod method;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.Play(method.animation);   
+    }
+}
