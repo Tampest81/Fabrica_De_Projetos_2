55473141908fa41eb281c22ee665b6f46ea3bf3d
Alessandro <maquexxx@gmail.com>
2020-11-02 19:04:50 -0200

    uk
    gykgvkyg

--- Assets/Code/HUD/TutorialBox.cs
+++ Assets/Code/HUD/TutorialBox.cs
@@ -6,7 +6,6 @@ public class TutorialBox : MonoBehaviour
 {
     public GameObject Boxes;
 
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
